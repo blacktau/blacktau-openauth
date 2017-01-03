f68840a7e97e7dc2ba9924600b4f22d0022d1e8d
Sean Garrett <sean@blacktau.com>
2017-01-03 10:31:50 +0000

    Added SimpleInjector DI Container package Extracted DIContainreBase to allow for simplified DI container packages Updated to dotnet core 1.1.0

--- src/Blacktau.OpenAuth.AspNet.SessionStateStorage/SessionStateStorageManager.cs
+++ src/Blacktau.OpenAuth.AspNet.SessionStateStorage/SessionStateStorageManager.cs
@@ -1,5 +1,7 @@
 ﻿namespace Blacktau.OpenAuth.AspNet.SessionStateStorage
 {
+    using System.Text;
+
     using Blacktau.OpenAuth.AspNet.Authorization.Interfaces;
 
     using Microsoft.AspNetCore.Http;
@@ -10,14 +12,37 @@
     {
         public T Retrieve<T>(HttpContext httpContext, string key)
         {
-            var valueString = httpContext.Session.GetString(key);
+            var valueString = GetString(httpContext.Session, key);
             return JsonConvert.DeserializeObject<T>(valueString);
         }
 
         public void Store<T>(HttpContext httpContext, string key, T value)
         {
             var valueString = JsonConvert.SerializeObject(value);
-            httpContext.Session.SetString(key, valueString);
+            SetString(httpContext.Session, key, valueString);
+        }
+
+        private static byte[] GetValue(ISession session, string key)
+        {
+            byte[] numArray;
+            session.TryGetValue(key, out numArray);
+            return numArray;
+        }
+
+        private static string GetString(ISession session, string key)
+        {
+            var bytes = GetValue(session, key);
+            if (bytes == null)
+            {
+                return null;
+            }
+
+            return Encoding.UTF8.GetString(bytes);
+        }
+
+        private static void SetString(ISession session, string key, string value)
+        {
+            session.Set(key, Encoding.UTF8.GetBytes(value));
         }
     }
 }
---
+++ src/Blacktau.OpenAuth.Client.Containers.SimpleInjector/Bootstrapper.cs
@@ -0,0 +1,24 @@
+﻿namespace Blacktau.OpenAuth.Client.Containers.SimpleInjector
+{
+    using Blacktau.OpenAuth.Client.Interfaces;
+    using Blacktau.OpenAuth.Client.Interfaces.VersionOneA;
+    using Blacktau.OpenAuth.Client.VersionOneA;
+
+    using global::SimpleInjector;
+
+    public class Bootstrapper
+    {
+        public static void RegisterServices(Container container)
+        {
+            container.Register<IClock, SystemClock>(Lifestyle.Singleton);
+            container.Register<IHttpClientFactory, HttpClientFactory>(Lifestyle.Singleton);
+            container.Register<INonceFactory, NonceFactory>(Lifestyle.Singleton);
+            container.Register<ITimeStampFactory, TimeStampFactory>(Lifestyle.Singleton);
+            container.Register<IAuthorizationSigner, AuthorizationSigner>(Lifestyle.Singleton);
+            container.Register<IAuthorizationParametersGenerator, AuthorizationParametersGenerator>(Lifestyle.Singleton);
+            container.Register<IOpenAuthVersionOneAAuthorizationHeaderGenerator, VersionOneA.AuthorizationHeaderGenerator>();
+            container.Register<IOpenAuthVersionTwoAuthorizationHeaderGenerator, VersionTwo.AuthorizationHeaderGenerator>();
+            container.Register<IOpenAuthClientFactory, OpenAuthClientFactory>(Lifestyle.Singleton);
+        }
+    }
+}
